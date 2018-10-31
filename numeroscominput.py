08900b2fc546cfadbc7f555dc6f68d239e041890
realvesd <43302742+realvesd@users.noreply.github.com>
2018-10-30 21:02:51 -0300

    Create numeroscominput.py

---
+++ numeroscominput.py
@@ -0,0 +1,9 @@
+numeros = [int(input('informe um numero: ')),
+           int(input('informe um numero: ')),
+           int(input('informe um numero: ')),
+           int(input('informe um numero: ')),
+           int(input('informe um numero: ')),
+           int(input('informe um numero: '))]
+for numero in numeros:
+  if numero >= 0:
+    print(numero)
